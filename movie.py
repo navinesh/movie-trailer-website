fd8abef15302d3e0b31588c86eae4cf474f80b1d
navinesh <navinesh.chand@gmail.com>
2015-10-09 17:00:53 +1200

    Cleaned code

--- movie.py
+++ movie.py
@@ -1,11 +1,8 @@
-import webbrowser
-
 class Movie():
-    """This is the Movie class to create movie instances"""
-    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+    """Movie class to create movie instances"""
+    def __init__(self, movie_title, movie_director, movie_starring, poster_image_url, trailer_youtube_url):
         self.title = movie_title
+        self.director = movie_director
+        self.starring = movie_starring
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-        def show_trailer(self):
-            webbrowser.open(self.trailer_youtube_url)
