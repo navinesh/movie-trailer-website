a8ce0509056575f7aef915031a72015c8a04e4d8
navinesh <navinesh.chand@gmail.com>
2015-10-12 10:49:23 +1200

    Delete movie.py

--- movie.py
+++
@@ -1,8 +0,0 @@
-class Movie():
-    """Movie class to create movie instances"""
-    def __init__(self, movie_title, movie_director, movie_starring, poster_image_url, trailer_youtube_url):
-        self.title = movie_title
-        self.director = movie_director
-        self.starring = movie_starring
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
